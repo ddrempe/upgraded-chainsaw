9704066d2a4ff1f0b9c0a6376f1988fe172f42c4
Damir Drempetić <ddrempe@gmail.com>
2017-07-07 11:42:29 +0200

    Event Binding

--- upgraded-chainsaw/src/app/servers/servers.component.ts
+++ upgraded-chainsaw/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,4 +22,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
