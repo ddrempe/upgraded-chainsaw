0166baafdef488f2956d9a7747c344be1fac8477
Damir Drempetić <ddrempe@gmail.com>
2017-07-11 12:11:12 +0200

    Solution of first four tasks

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayIsEnabled = false;
+  displayToggleLogs = [''];
+  displayToggleLogsWithTimestamp = [''];
+  logCounter;
+
+  constructor() {
+    this.logCounter = 0;
+  }
+
+  enableDisplay() {
+    this.displayIsEnabled = (!this.displayIsEnabled);
+    this.logCounter++;
+    this.displayToggleLogs.push(this.logCounter);
+    this.displayToggleLogsWithTimestamp.push(new Date().toLocaleString());
+  }
+
+  getBkgColor(log) {
+    if (log > 5) {
+      return 'blue';
+    }
+  }
 }
