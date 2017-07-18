b202fa61099bbb23de3e0cd9fdca966b78141247
Damir Drempetić <ddrempe@gmail.com>
2017-07-18 09:04:37 +0200

    Added inline template and styling

--- basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
+++ basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
-  templateUrl: './success-alert.component.html'
+  template: `
+    <div class="alert alert-success">
+      <strong>Success!</strong> Indicates a successful or positive action.
+    </div>
+  `,
+  styles: [
+      `
+      div {
+        font-size: 20px;
+      }
+    `
+  ]
 })
 
 export class SuccessAlertComponent{
