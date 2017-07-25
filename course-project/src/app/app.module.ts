26121ea7da68cce49cde65af8d1f05ed2026f559
Damir Drempetić <ddrempe@gmail.com>
2017-07-25 13:25:41 +0200

    S6C80 Allowing the User to Add Ingredients to the Shopping List

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +25,9 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingEditComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
